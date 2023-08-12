0386fcea498c0414a2893e47059061a86404ca61
zhangluduo@microtrust.com.cn <zhangluduo@microtrust.com.cn>
2023-08-12 09:36:44 +0800

    ~

--- src/pbkdf2.c
+++ src/pbkdf2.c
@@ -32,9 +32,9 @@ where:
 
 #if defined ZMCRYPTO_ALGO_PBKDF2
 
-	#if !defined ZMCRYPTO_ALGO_HMAC
-		#error The macro 'ZMCRYPTO_ALGO_HMAC' is undefined.
-	#endif
+    #if !defined ZMCRYPTO_ALGO_HMAC
+        #error The macro 'ZMCRYPTO_ALGO_HMAC' is undefined.
+    #endif
 
     void pbkdf2_do_xor(uint8_t* buf, const uint8_t* mask, uint32_t count)
     {
@@ -43,35 +43,35 @@ where:
         }
     }
 
-	zmerror pbkdf2(
-		void*   (*hash_new)         (void),
-		void(*hash_free)        (void* ctx),
-		int32_t(*hash_digest_size) (void),
-		int32_t(*hash_block_size)  (void),
-		void(*hash_init)        (void* ctx),
-		void(*hash_starts)      (void* ctx),
-		void(*hash_update)      (void* ctx, uint8_t* data, uint32_t dlen),
-		void(*hash_final)       (void* ctx, uint8_t* output),
-		uint8_t* p, uint32_t plen, uint8_t* s, uint32_t slen, uint32_t c, uint8_t* dk, uint32_t dklen)
-	{
-		if (!hash_new || !hash_free || !hash_digest_size || !hash_block_size || !hash_init ||
-			!hash_starts || !hash_update || !hash_final || !p || !s || !dk){
-			return ZMCRYPTO_ERR_NULL_PTR;
-		}
+    zmerror pbkdf2(
+        void*   (*hash_new)        (void),
+        void(*hash_free)           (void* ctx),
+        int32_t(*hash_digest_size) (void),
+        int32_t(*hash_block_size)  (void),
+        void(*hash_init)           (void* ctx),
+        void(*hash_starts)         (void* ctx),
+        void(*hash_update)         (void* ctx, uint8_t* data, uint32_t dlen),
+        void(*hash_final)          (void* ctx, uint8_t* output),
+        uint8_t* p, uint32_t plen, uint8_t* s, uint32_t slen, uint32_t c, uint8_t* dk, uint32_t dklen)
+    {
+        if (!hash_new || !hash_free || !hash_digest_size || !hash_block_size || !hash_init ||
+            !hash_starts || !hash_update || !hash_final || !p || !s || !dk){
+            return ZMCRYPTO_ERR_NULL_PTR;
+        }
 
-		if (plen == 0 || slen == 0 || dklen == 0){
-			return ZMCRYPTO_ERR_INVALID_DSIZE;
-		}
+        if (plen == 0 || slen == 0 || dklen == 0){
+            return ZMCRYPTO_ERR_INVALID_DSIZE;
+        }
 
-		struct hmac_ctx _hmac_ctx;
-		hmac_init(&_hmac_ctx, hash_new, hash_free, hash_digest_size, hash_block_size, hash_init, hash_starts, hash_update, hash_final);
+        struct hmac_ctx _hmac_ctx;
+        hmac_init(&_hmac_ctx, hash_new, hash_free, hash_digest_size, hash_block_size, hash_init, hash_starts, hash_update, hash_final);
 
-		uint8_t digest[DIGEST_MAX_SIZE];
-		uint32_t hlen = hash_digest_size();
-		uint32_t loop = (dklen + (hlen - 1)) / hlen;
-		uint32_t offset = 0;
-		
-		for (uint32_t i = 0; i < loop; i++){
+        uint8_t digest[DIGEST_MAX_SIZE];
+        uint32_t hlen = hash_digest_size();
+        uint32_t loop = (dklen + (hlen - 1)) / hlen;
+        uint32_t offset = 0;
+        
+        for (uint32_t i = 0; i < loop; i++){
             hmac_starts(&_hmac_ctx, p, plen);
             hmac_update(&_hmac_ctx, s, slen);
             for (uint32_t j = 0; j < 4; j++)
@@ -86,8 +86,8 @@ where:
             }
             hmac_final(&_hmac_ctx, digest);
 
-			uint32_t seglen = (offset + hlen > dklen) ? (dklen - hlen * i) : (hlen);
-			zmcrypto_memcpy(dk + offset, digest, seglen);
+            uint32_t seglen = (offset + hlen > dklen) ? (dklen - hlen * i) : (hlen);
+            zmcrypto_memcpy(dk + offset, digest, seglen);
 
             for (uint32_t j = 1; j < c; j++)
             {
@@ -97,8 +97,8 @@ where:
                 pbkdf2_do_xor(dk + offset, digest, seglen);
             }
 
-			offset += seglen;
-		}
+            offset += seglen;
+        }
 
         return ZMCRYPTO_ERR_SUCCESSED;
     }
