7fd5eed298637726e950281de2700f4d38bc1142
zhangluduo@microtrust.com.cn <zhangluduo@microtrust.com.cn>
2023-09-22 11:07:34 +0800

    add test case for RC4

---
+++ tests/cpp/test_rc4.cpp
@@ -0,0 +1,152 @@
+/*
+ *  Copyright 2022 The ZmCrypto Project. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the License); you may
+ *  not use this file except in compliance with the License.
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * 
+ * Author: Zhang Luduo (zhangluduo@qq.com)
+ *   Date: Nov 2022
+ *   Home: https://zmcrypto.cn/
+ *         https://github.com/zhangluduo/
+ */
+
+#include "zmcryptosdk.h"
+#include "vector_file.h"
+#include "format_output.h"
+#include "time_stamp.h"
+#include "test_config.h"
+#include "test_rc4.h"
+#include <memory.h>
+
+#if defined TEST_FOR_CRYPTOPP
+    #include "cryptopp820/include/cryptlib.h"
+    #include "cryptopp820/include/secblock.h"
+    #include "cryptopp820/include/arc4.h"
+#endif
+
+void test_case_rc4(zmcrypto::sdk* _sdk)
+{
+    std::vector<key_val_vec> test_vec;
+    if (!read_vector_data(TEST_VECTOR_PATH "rc4.txt", test_vec)){
+        printf("read test vector data failed\n");
+        return;
+    }
+
+    for (size_t i = 0; i < test_vec.size(); i++){
+        std::string algorithm, key, plaintext, ciphertext, comment;
+        if (!get_key_val_pair(test_vec, i, "comment", comment)){
+        }
+        if (!get_key_val_pair(test_vec, i, "algorithm", algorithm)){
+            printf("get key-value pair failed: algorithm\n");
+            return;
+        }
+        if (!get_key_val_pair(test_vec, i, "key", key)){
+            printf("get key-value pair failed: key\n");
+            return;
+        }
+        if (!get_key_val_pair(test_vec, i, "plaintext", plaintext)){
+            printf("get key-value pair failed: plaintext\n");
+            return;
+        }
+        if (!get_key_val_pair(test_vec, i, "ciphertext", ciphertext)){
+            printf("get key-value pair failed: ciphertext\n");
+            return;
+        }
+
+        #if defined ZMCRYPTO_ALGO_RC4
+        /*Encrypt*/
+        {
+            uint8_t* output = new uint8_t[ciphertext.size()];
+
+            zmerror err;
+            CONTEXT_TYPE_PTR(rc4) ctx = _sdk->zm_rc4_new();
+            _sdk->zm_rc4_init(ctx);
+            err = _sdk->zm_rc4_set_ekey(ctx, (uint8_t*)key.c_str(), key.size());
+            if (ZMCRYPTO_IS_ERROR(err)){
+                delete[] output;
+                output = NULL;
+                printf ("%s\n", _sdk->zm_error_str(err));
+                _sdk->zm_rc4_free (ctx);
+                return;
+            }
+            _sdk->zm_rc4_encrypt(ctx, (uint8_t*)plaintext.c_str(), plaintext.size(), output);
+            _sdk->zm_rc4_free(ctx);
+
+            if (ciphertext == std::string((char*)output, ciphertext.length())){
+                format_output("%s encryption by ZmCrypto|%s passed\n", algorithm.c_str(), comment.c_str());
+            }
+            else{
+                format_output("%s encryption by ZmCrypto|%s failed\n", algorithm.c_str(), comment.c_str());
+            }
+            delete[] output;
+            output = NULL;
+        }
+        /*Decrypt*/
+        {
+            uint8_t* output = new uint8_t[plaintext.size()];
+
+            zmerror err;
+            CONTEXT_TYPE_PTR(rc4) ctx = _sdk->zm_rc4_new();
+            _sdk->zm_rc4_init(ctx);
+            err = _sdk->zm_rc4_set_dkey(ctx, (uint8_t*)key.c_str(), key.size());
+            if (ZMCRYPTO_IS_ERROR(err)){
+                delete[] output;
+                output = NULL;
+                printf ("%s\n", _sdk->zm_error_str(err));
+                _sdk->zm_rc4_free (ctx);
+                return;
+            }
+            _sdk->zm_rc4_decrypt(ctx, (uint8_t*)ciphertext.c_str(), ciphertext.size(), output);
+            _sdk->zm_rc4_free(ctx);
+
+            if (plaintext == std::string((char*)output, plaintext.length())){
+                format_output("%s decryption by ZmCrypto|%s passed\n", algorithm.c_str(), comment.c_str());
+            }
+            else{
+                format_output("%s decryption by ZmCrypto|%s failed\n", algorithm.c_str(), comment.c_str());
+            }
+            delete[] output;
+            output = NULL;
+        }
+        
+        #endif
+
+        #if defined TEST_FOR_CRYPTOPP
+        /*Encrypt*/
+        {
+            uint8_t* output = new uint8_t[ciphertext.size()];
+            CryptoPP::Weak1::ARC4 arc4;
+            arc4.SetKey ((uint8_t*)key.c_str(), key.size());
+            arc4.ProcessData (output, (uint8_t*)plaintext.c_str(), plaintext.size());
+
+            if (ciphertext == std::string((char*)output, ciphertext.length())){
+                format_output("%s encryption by Crypto++|%s passed\n", algorithm.c_str(), comment.c_str());
+            }
+            else{
+                format_output("%s encryption by Crypto++|%s failed\n", algorithm.c_str(), comment.c_str());
+            }
+            delete[] output;
+            output = NULL;
+        }
+        /*Decrypt*/
+        {
+            uint8_t* output = new uint8_t[ciphertext.size()];
+            CryptoPP::Weak1::ARC4 arc4;
+            arc4.SetKey ((uint8_t*)key.c_str(), key.size());
+            arc4.ProcessData (output, (uint8_t*)ciphertext.c_str(), ciphertext.size());
+
+            if (plaintext == std::string((char*)output, plaintext.length())){
+                format_output("%s decryption by Crypto++|%s passed\n", algorithm.c_str(), comment.c_str());
+            }
+            else{
+                format_output("%s decryption by Crypto++|%s failed\n", algorithm.c_str(), comment.c_str());
+            }
+            delete[] output;
+            output = NULL;
+        }
+        #endif
+    }
+}
