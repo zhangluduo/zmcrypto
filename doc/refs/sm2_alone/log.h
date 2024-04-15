857eac72a8379e217d46e4b309deb76203fe16c3
zhangluduo@microtrust.com.cn <zhangluduo@microtrust.com.cn>
2024-04-15 15:26:47 +0800

    code backup

--- doc/refs/sm2_alone/log.h
+++
@@ -1,8 +0,0 @@
-
-#if !defined _LOG_HEADER_H
-#define _LOG_HEADER_H
-
-#define LOG(...) logMsg(__FILE__, __PRETTY_FUNCTION__, __LINE__, __VA_ARGS__);
-void logMsg(const char* const file, const char* const fn, int ln, const char* const fmt, ...);
-
-#endif
